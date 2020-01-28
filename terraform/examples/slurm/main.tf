b0e3701cee410c298f600f4d259d3a7aa3e83a11
John Garbutt <john@johngarbutt.com>
2020-01-28 13:32:23 +0000

    Add volumes onto login node

--- terraform/examples/slurm/main.tf
+++ terraform/examples/slurm/main.tf
@@ -20,3 +20,36 @@ module "cluster" {
   compute_count = 1
   network_name = "demo-vxlan"
 }
+
+
+resource "openstack_blockstorage_volume_v3" "mgs" {
+  name        = "mgs"
+  description = "management volume"
+  size        = 5
+}
+resource "openstack_compute_volume_attach_v2" "va_mgs" {
+  instance_id = module.cluster.login_id
+  volume_id   = openstack_blockstorage_volume_v3.mgs.id
+}
+
+resource "openstack_blockstorage_volume_v3" "mdt1" {
+  name        = "mdt1"
+  description = "metadata volume 1"
+  size        = 5
+}
+resource "openstack_compute_volume_attach_v2" "va_mdt1" {
+  instance_id = module.cluster.login_id
+  volume_id   = openstack_blockstorage_volume_v3.mdt1.id
+  depends_on  = ["openstack_compute_volume_attach_v2.va_mgs"]
+}
+
+resource "openstack_blockstorage_volume_v3" "ost1" {
+  name        = "ost1"
+  description = "storage target 1"
+  size        = 50
+}
+resource "openstack_compute_volume_attach_v2" "va_ost1" {
+  instance_id = module.cluster.login_id
+  volume_id   = openstack_blockstorage_volume_v3.ost1.id
+  depends_on  = ["openstack_compute_volume_attach_v2.va_mdt1"]
+}
--- terraform/modules/cluster/outputs.tf
+++ terraform/modules/cluster/outputs.tf
@@ -2,3 +2,8 @@ output "public_ip" {
   value = openstack_networking_floatingip_v2.fip_1.address
   description = "Public IP for OpenHPC login node"
 }
+
+output "login_id" {
+  value = openstack_compute_instance_v2.login.id
+  description = "instance_id for the login node"
+}
